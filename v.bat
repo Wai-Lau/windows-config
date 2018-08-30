0e7f1f6f6b39d845736b669add6b499a83b05cc9
Wai <12164553+Wai-Lau@users.noreply.github.com>
2018-08-29 23:39:43 -0400

    Reduce length of volume beeps

--- v.bat
+++ v.bat
@@ -1,7 +1,7 @@
 @echo off
 SET /A VOLUME = %1 * 65535 / 100
 nircmd setvolume 0 %VOLUME% %VOLUME%
-nircmd beep 262 600
-nircmd beep 294 600
-nircmd beep 262 600
+nircmd beep 262 400
+nircmd beep 294 400
+nircmd beep 262 400
 @echo on
